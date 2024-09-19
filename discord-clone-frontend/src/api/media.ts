5723710dbaf4e152301da09fee99bf0771f08760
danh3105 <chidanh0502@gmail.com>
2024-09-19 21:07:16 +0700

    update media upload

--- discord-clone-frontend/src/api/media.ts
+++ discord-clone-frontend/src/api/media.ts
@@ -1,19 +1,17 @@
-import { server } from './base';
+import { server } from "./base";
 
 export const uploadMedia = async (fileMe: File): Promise<string> => {
   const formData = new FormData();
-  formData.append('mediaFile', fileMe);
+  formData.append("mediaFile", fileMe);
 
   try {
-    const response = await server.post('/api/media/upload', formData, {
+    const response = await server.post("/api/media/upload", formData, {
       headers: {
-        'Content-Type': 'multipart/form-data',
+        "Content-Type": "multipart/form-data",
       },
     });
-
     return response.data; // Assuming the response contains the uploaded file's URL or identifier
   } catch (error) {
-    console.error('Error uploading media:', error);
     throw error; // Rethrow the error for further handling
   }
 };
